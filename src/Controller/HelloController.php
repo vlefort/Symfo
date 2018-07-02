d8425303e0cfe1d748935f8d6ad2ba8873f32d79
BeardTech <pierre.met3@gmail.com>
2018-07-02 11:07:16 +0200

    Initial commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HelloController extends Controller
+{
+    /**
+     * @Route("/hello/{name}", defaults={"name"="World"})
+     */
+    public function hello($name)
+    {
+        return $this->render('hello/index.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
