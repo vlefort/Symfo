20589dc6a6a2a5899e341f165119b8e5040c7437
BeardTech <pierre.met3@gmail.com>
2018-07-02 12:33:44 +0200

    gestion assets + bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
+// the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+
+    // read main.js     -> output as web/build/app.js
+    .addEntry('app', './assets/app.js')
+    // read global.scss -> output as web/build/global.css
+    .addStyleEntry('global', './assets/global.scss')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
+    .enableSassLoader()
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
 
@@ -14,12 +19,6 @@ Encore
     // .addEntry('js/app', './assets/js/app.js')
     // .addStyleEntry('css/app', './assets/css/app.scss')
 
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
     .autoProvidejQuery()
-;
 
 module.exports = Encore.getWebpackConfig();
