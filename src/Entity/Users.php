7f2d378f2bad6657b235592b61ead0d0c24ef930
BeardTech <pierre.met3@gmail.com>
2018-07-03 15:11:07 +0200

    maj

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -30,7 +30,7 @@ class Users implements UserInterface, \Serializable
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=254, unique=true)
+     * @ORM\Column(type="string", length=150, unique=true)
      */
     private $email;
 
