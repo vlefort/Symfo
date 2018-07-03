da03405dae90aff4a02ebb31848a01478f333c0f
BeardTech <pierre.met3@gmail.com>
2018-07-03 09:19:54 +0200

    gestion user

---
+++ src/Form/UtilisateursType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateurs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Nom')
+            ->add('Login')
+            ->add('Mdp')
+            ->add('admin')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateurs::class,
+        ]);
+    }
+}
